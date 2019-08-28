db0adb6bd70448fd95fefa87b61a3cac51be3603
Dao Quang Huy <daoquanghuy.uet.140598@gmail.com>
2019-08-28 22:36:05 +0700

    Add module FaceAlignment

---
+++ FaceAlignment/face_align.py
@@ -0,0 +1,54 @@
+from PIL import Image
+from FaceAlignment.detector import FaceDetectAlign
+from FaceAlignment.align_trans import get_reference_facial_points, warp_and_crop_face
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+
+class FaceAlignment:
+
+    def __init__(self):
+        self.FaceDetectorAlign = FaceDetectAlign()
+
+    def run(self,face):
+        crop_size = 112
+        scale = crop_size / 112.
+        reference = get_reference_facial_points(default_square=True) * scale
+
+        face = Image.fromarray(face)
+
+        _, landmarks = self.FaceDetectorAlign.runOn(face)
+
+        img_warped = None
+
+        if len(landmarks) > 0:  # If the landmarks cannot be detected, the img will be discarded
+            facial5points = [[landmarks[0][j], landmarks[0][j + 5]] for j in range(5)]
+            warped_face = warp_and_crop_face(np.array(face), facial5points, reference, crop_size=(crop_size, crop_size))
+            img_warped = Image.fromarray(warped_face)
+
+        return img_warped
+
+# def align_face(face):
+#     crop_size = 112
+#     scale = crop_size / 112.
+#     reference = get_reference_facial_points(default_square = True) * scale
+#
+#     face = Image.fromarray(face)
+#
+#     _, landmarks = detect_faces(face)
+#
+#     if len(landmarks) > 0: # If the landmarks cannot be detected, the img will be discarded
+#         facial5points = [[landmarks[0][j], landmarks[0][j + 5]] for j in range(5)]
+#         warped_face = warp_and_crop_face(np.array(face), facial5points, reference, crop_size=(crop_size, crop_size))
+#         img_warped = Image.fromarray(warped_face)
+#
+#     return img_warped
+
+if __name__ == '__main__':
+
+    img = cv2.imread("E:\Source\PyFace\FaceChecker\portrait-photography.jpg")
+    img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+    img = cv2.resize(img,(112,112))
+    # face = align_face(img)
+    # plt.imshow(face)
+    # plt.show()
--- main.py
+++ main.py
@@ -3,14 +3,26 @@ from FaceChecker.HeadPoseEstimator.Headpose import HeadposeEstimation
 from FaceChecker.config import FINAL_WEIGHTS_PATH,FACE_DETECTOR_MODEL_PATH
 from FaceChecker.FaceDetector.FaceDetector import FaceDetector
 from FaceChecker.NoiseEstimator.NoiseEstimator import NoiseEstimator
+from FaceAlignment.face_align import FaceAlignment
+import cv2
+import matplotlib.pyplot as plt
+# head_pose_estimator = HeadposeEstimation(FINAL_WEIGHTS_PATH)
+# face_detector = FaceDetector(model_path=FACE_DETECTOR_MODEL_PATH)
+# noise_estimator = NoiseEstimator()
+#
+# face_checker = FaceChecker(head_pose_estimator,face_detector,noise_estimator)
+#
+# video_path = 'FaceChecker/test.mp4'
+#
+# face_checker.test_on_video(video_path)
 
+img = cv2.imread("FaceChecker/BZ7LGZSXCFHOXAN2XCTAM4QRPQ.jpg")
+img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
 
-head_pose_estimator = HeadposeEstimation(FINAL_WEIGHTS_PATH)
-face_detector = FaceDetector(model_path=FACE_DETECTOR_MODEL_PATH)
-noise_estimator = NoiseEstimator()
+face_alignment = FaceAlignment()
 
-face_checker = FaceChecker(head_pose_estimator,face_detector,noise_estimator)
+face = face_alignment.run(img)
 
-video_path = 'FaceChecker/test.mp4'
+plt.imshow(face)
 
-face_checker.test_on_video(video_path)
+plt.show()
